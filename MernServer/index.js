115d74b54a36de85353c5867c96d11ce3c5a1a01
Anand Kumar Bharti <anandkr369@gmail.com>
2024-08-01 23:14:17 +0530

    updating razorpay details

--- MernServer/index.js
+++ MernServer/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const Razorpay = require('razorpay');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) =>{
-    res.send("hello");
+app.get('/', (req, res) => {
+  res.send("Hello Mongoose!");
 });
+
 const uri = process.env.MONGODB_URL;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,88 +23,92 @@ const client = new MongoClient(uri, {
   }
 });
 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
 async function run() {
   try {
     await client.connect();
-
     const bookCollections = client.db("BookInventory").collection("books");
 
-    app.post("/upload-book", async(req, res) => {
-        try {
-            const data = req.body;
-            const result = await bookCollections.insertOne(data);
-            res.send(result);
-        } catch (error) {
-            console.error("Error inserting book:", error);
-            res.status(500).send("Error inserting book");
-        }
+    app.post("/upload-book", async (req, res) => {
+      try {
+        const data = req.body;
+        const result = await bookCollections.insertOne(data);
+        res.send(result);
+      } catch (error) {
+        console.error("Error inserting book:", error);
+        res.status(500).send("Error inserting book");
+      }
     });
 
-    //http://localhost:5000/all-books
-    app.get("/all-books", async(req, res) =>{
-      const books =  bookCollections.find();
+    app.get("/all-books", async (req, res) => {
+      const books = bookCollections.find();
       const result = await books.toArray();
       res.send(result);
-    })
+    });
 
-    //update a book data : use patch or update method
-    app.patch("/book/:id", async(req, res) => {
+    app.patch("/book/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const updateBookData = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-  
-      const updateDoc = {
-          $set: {
-              ...updateBookData
-          }
-      };
-  
+
+      const updateDoc = { $set: { ...updateBookData } };
+
       try {
-          const result = await bookCollections.updateOne(filter, updateDoc, options);
-          res.send(result);
+        const result = await bookCollections.updateOne(filter, updateDoc, options);
+        res.send(result);
       } catch (error) {
-          console.error("Error updating book:", error);
-          res.status(500).send("Error updating book");
+        console.error("Error updating book:", error);
+        res.status(500).send("Error updating book");
       }
-  });
+    });
 
-  //delete a book data
-  app.delete("/book/:id", async(req, res) =>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const result = await bookCollections.deleteOne(filter);
-    res.send(result);
-  })
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(filter);
+      res.send(result);
+    });
 
-
-  //find by category  :: http://localhost:5000/all-books?category=Gothic%20Fiction
-  app.get("/all-books", async (req, res) => {
-    let query = {};
-    if (req.query?.category) {
-        query = {category: req.query.category };
-    }
-    try {
+    app.get("/all-books", async (req, res) => {
+      let query = {};
+      if (req.query?.category) {
+        query = { category: req.query.category };
+      }
+      try {
         const result = await bookCollections.find(query).toArray();
         res.send(result);
-    } catch (error) {
+      } catch (error) {
         console.error("Error retrieving book:", error);
         res.status(500).send("Error retrieving book");
-    }
-});
+      }
+    });
 
-//to get single book data
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.findOne(filter);
+      res.send(result);
+    });
 
-  app.get("/book/:id", async(req,res) =>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)}
-    const result = await bookCollections.findOne(filter);
-    res.send(result);
-  })
-
-
-
+    app.post('/create-order', async (req, res) => {
+      const { amount, currency, receipt } = req.body;
+      try {
+        const order = await razorpay.orders.create({
+          amount: amount * 100,
+          currency,
+          receipt,
+        });
+        res.json(order);
+      } catch (error) {
+        console.error("Error creating order:", error);
+        res.status(500).send("Error creating order");
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -115,5 +121,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}....`);
+  console.log(`Listening to port ${PORT}....`);
 });
